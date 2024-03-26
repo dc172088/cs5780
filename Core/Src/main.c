209921e6317d69efe9945939326626753bee13ab
Derek Che <dche@shamie.cloud>
2024-03-26 09:55:12 -0600

    Finished PI loop

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,118 +1,119 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2024 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "motor.h"
+#include "stm32f0xx.h"
+
+/* -------------------------------------------------------------------------------------------------------------
+ *  Global Variable Declarations
+ *  -------------------------------------------------------------------------------------------------------------
  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+volatile uint32_t debouncer;
 
-#include "dtimer.h"
-#include "gpio.h"
-#include "i2c.h"
-#include "usart.h"
-#include "adc.h"
-#include "dac.h"
-
-void SystemClock_Config(void);
-
-/**
- * @brief  The application entry point.
- * @retval int
+/* -------------------------------------------------------------------------------------------------------------
+ *  Miscellaneous Core Functions
+ *  -------------------------------------------------------------------------------------------------------------
  */
-int main(void) {
-    HAL_Init();
 
-    /* Configure the system clock */
-    SystemClock_Config();
+void LED_init(void) {
+    // Initialize PC8 and PC9 for LED's
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;                          // Enable peripheral clock to GPIOC
+    GPIOC->MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0;  // Set PC8 & PC9 to outputs
+    GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);    // Set to push-pull output type
+    GPIOC->OSPEEDR &= ~((GPIO_OSPEEDR_OSPEEDR8_0 | GPIO_OSPEEDR_OSPEEDR8_1) |
+                        (GPIO_OSPEEDR_OSPEEDR9_0 | GPIO_OSPEEDR_OSPEEDR9_1));  // Set to low speed
+    GPIOC->PUPDR &= ~((GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR8_1) |
+                      (GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR9_1));  // Set to no pull-up/down
+    GPIOC->ODR &= ~(GPIO_ODR_8 | GPIO_ODR_9);                        // Shut off LED's
+}
 
-    /* Initialize all configured peripherals */
-    gpio_init();
-    gpio_configure_pins();
+void button_init(void) {
+    // Initialize PA0 for button input
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;                                       // Enable peripheral clock to GPIOA
+    GPIOA->MODER &= ~(GPIO_MODER_MODER0_0 | GPIO_MODER_MODER0_1);            // Set PA0 to input
+    GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR0_0 | GPIO_OSPEEDR_OSPEEDR0_1);  // Set to low speed
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_1;                                     // Set to pull-down
+}
 
-    dac_init();
+/* Called by SysTick Interrupt
+ * Performs button debouncing, changes wave type on button rising edge
+ * Updates frequency output from ADC value
+ */
+void HAL_SYSTICK_Callback(void) {
+    // Remember that this function is called by the SysTick interrupt
+    // You can't call any functions in here that use delay
 
+    debouncer = (debouncer << 1);
+    if (GPIOA->IDR & (1 << 0)) {
+        debouncer |= 0x1;
+    }
+
+    if (debouncer == 0x7FFFFFFF) {
+        switch (target_rpm) {
+            case 80:
+                target_rpm = 50;
+                break; 
+            case 50:
+                target_rpm = 81;
+                break;
+            case 0:
+                target_rpm = 80;
+                break;
+            default:
+                target_rpm = 0;
+                break;
+        }
+    }
+}
+
+/* -------------------------------------------------------------------------------------------------------------
+ * Main Program Code
+ *
+ * Starts initialization of peripherals
+ * Blinks green LED (PC9) in loop as heartbeat
+ * -------------------------------------------------------------------------------------------------------------
+ */
+volatile uint32_t encoder_count = 0;
+
+int main(int argc, char* argv[]) {
+    debouncer = 0;  // Initialize global variables
+    HAL_Init();     // Initialize HAL
+    LED_init();     // Initialize LED's
+    button_init();  // Initialize button
+
+    motor_init();  // Initialize motor code
+
+    uint8_t i = 0;
     while (1) {
-        // Write to DAC
-        dac_write();
+        GPIOC->ODR ^= GPIO_ODR_9;  // Toggle green LED (heartbeat)
+        encoder_count = TIM2->CNT;
+        HAL_Delay(128);  // Delay 1/8 second
+        
+        if (i >= 0 && i < 40) {
+            target_rpm = 20;
+        }
+        if (i >= 40 && i < 80) {
+            target_rpm = 40;
+        }
+        if (i >= 80 && i < 120) {
+            target_rpm = 60;
+        }
+        if (i >= 120 && i < 160) {
+            target_rpm = 80;
+        }
+        if (i >= 160 && i < 200) {
+            target_rpm = 100;
+        }
+        if (i >= 200 && i < 240) {
+            target_rpm = 120;
+        }
+        if (i >= 240) {
+            target_rpm = 0;
+        }
 
-        // Delay by 1ms
-        HAL_Delay(1);
+        i++;
     }
 }
 
-/**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** Initializes the CPU, AHB and APB buses clocks
-     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
-        Error_Handler();
-    }
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-    /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state */
-    __disable_irq();
-    while (1) {
-    }
-    /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef USE_FULL_ASSERT
-/**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
-void assert_failed(uint8_t* file, uint32_t line) {
-    /* USER CODE BEGIN 6 */
-    /* User can add his own implementation to report the file name and line number,
-       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-    /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+// ----------------------------------------------------------------------------
